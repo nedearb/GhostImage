48392696633fd6d64117f4b26d43570af71ae920
Emily Atlee <git@emilyatlee.com>
2021-03-23 19:23:53 -0700

    add actual size menu item and update opacity minimum

--- GhostImage/AppDelegate.swift
+++ GhostImage/AppDelegate.swift
@@ -16,16 +16,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var menuItemDecOpacity: NSMenuItem!
     @IBOutlet weak var menuItemIncOpacity: NSMenuItem!
+    @IBOutlet weak var menuItemFullOpacity: NSMenuItem!
+    
+    
+    @IBOutlet weak var menuItemMaintainAspectRatio: NSMenuItem!
+    @IBOutlet weak var menuItemActualSize: NSMenuItem!
+    
+    var actualSize: Bool = false;
+    var maintainAspectRatio: Bool = true;
+    var opacityPercent: Int = 100;
     
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Init
         
-        window.alphaValue = 1.0;
         updateOpacityItemsEnabled();
         
         mainImageView.imageAlignment = NSImageAlignment.alignCenter;
-        setMaintainAspectRatio(true);
+        updateImageScaling();
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -56,42 +64,61 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
     }
     
-    func setMaintainAspectRatio(_ maintain: Bool) {
-        if(maintain){
-            mainImageView.imageScaling = NSImageScaling.scaleProportionallyUpOrDown;
-        }else{
-            mainImageView.imageScaling = NSImageScaling.scaleAxesIndependently;
+    func boolToState(_ input: Bool) -> NSControl.StateValue {
+       return input ? NSControl.StateValue.on : NSControl.StateValue.off
+    }
+    
+    func updateImageScaling() {
+        if (actualSize) {
+            mainImageView.imageScaling = NSImageScaling.scaleNone;
+        } else {
+            if (maintainAspectRatio) {
+                mainImageView.imageScaling = NSImageScaling.scaleProportionallyUpOrDown;
+            } else {
+                mainImageView.imageScaling = NSImageScaling.scaleAxesIndependently;
+            }
         }
+        
+        menuItemActualSize.state = boolToState(actualSize);
+        
+        menuItemMaintainAspectRatio.isEnabled = !actualSize;
+        menuItemMaintainAspectRatio.state = boolToState(actualSize || maintainAspectRatio);
+        
     }
     
     func updateOpacityItemsEnabled(){
-        menuItemIncOpacity.isEnabled = window.alphaValue < 1.0;
-        menuItemDecOpacity.isEnabled = window.alphaValue > 0.1;
+        menuItemIncOpacity.isEnabled = opacityPercent < 100;
+        menuItemDecOpacity.isEnabled = opacityPercent > 10;
+        menuItemFullOpacity.isEnabled = opacityPercent != 100;
+        window.alphaValue = CGFloat(opacityPercent) / 100.0;
     }
     
     @IBAction func onMenuAspectRatio(_ sender: NSMenuItem) {
-        let turnOn = sender.state == NSControl.StateValue.off;
-        sender.state = turnOn ? NSControl.StateValue.on : NSControl.StateValue.off;
-        
-        setMaintainAspectRatio(turnOn);
+        maintainAspectRatio = !maintainAspectRatio;
+        updateImageScaling();
+    }
+    
+    @IBAction func onMenuActualSize(_ sender: NSMenuItem) {
+        actualSize = !actualSize;
+        updateImageScaling();
     }
     
     @IBAction func onMenuIncOpacity(_ sender: NSMenuItem) {
-        if(window.alphaValue < 1.0){
-            window.alphaValue += 0.1;
+        if(opacityPercent < 100){
+            opacityPercent = min(opacityPercent+10, 100);
         }
         updateOpacityItemsEnabled();
     }
 
     @IBAction func onMenuDecOpacity(_ sender: NSMenuItem) {
-        if(window.alphaValue > 0.1){
-            window.alphaValue -= 0.1;
+        if(opacityPercent > 10){
+            opacityPercent = max(opacityPercent-10, 10);
         }
         updateOpacityItemsEnabled();
     }
     
     @IBAction func onMenuFullOpacity(_ sender: NSMenuItem) {
-        window.alphaValue = 1.0;
+        opacityPercent = 100;
         updateOpacityItemsEnabled();
     }
 }
